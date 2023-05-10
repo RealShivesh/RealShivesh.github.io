f160e72326cf8b0e5eaa7284b1be9ecb4dc76a98
Shivesh Sinha <realshivesh@gmail.com>
2023-05-10 12:00:55 +0530

    creates a footer and adds the year

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Footer from './components/Footer'
 import WorkInProgress from './components/WorkInProgress'
 
 function App() {
   return (
     <>
       <WorkInProgress />
+      <Footer />
     </>
   )
 }
