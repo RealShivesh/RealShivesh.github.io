2920035822afd37b05c61cb573a991d87e6a2538
Shivesh <realshivesh@gmail.com>
2023-07-21 15:54:22 +0530

    build 1.0.2

--- src/components/WorkInProgress.jsx
+++ src/components/WorkInProgress.jsx
@@ -1,11 +1,14 @@
-import TableFlip from '../assets/table_flip.png'
+import { WIP } from '../assets/svg'
+import Socials from './Socials'
+import '../assets/styles/wip.scss'
 
 const WorkInProgress = () => {
   return (
     <>
       <h1>On the horizon</h1>
       <p>A work in progress, won&apos;t be an endless process</p>
-      <img src={TableFlip} alt="" />
+      <img src={WIP} alt="" />
+      <Socials />
     </>
   )
 }
