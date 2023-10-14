ef1fccf3962f79601dc6f71650f100dfb3251a24
Shivesh <realshivesh@gmail.com>
2023-10-14 09:08:33 +0530

    update the linkedin icon typo error

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,5 +1,5 @@
 import Instagram from './instagram.svg'
-import LinkedIn from './linkedIn.svg'
+import LinkedIn from './linkedin.svg'
 import Github from './github.svg'
 import WIP from './wip.svg'
 
