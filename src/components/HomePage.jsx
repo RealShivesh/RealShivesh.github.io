240311766f38fd7660f0b5732436bbea4d34a41c
Shivesh <realshivesh@gmail.com>
2024-11-03 20:13:03 +0530

    add content to homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,15 +1,33 @@
 const HomePage = () => {
   return (
-    <div>
-      {' '}
-      <div>Shivesh Sinha</div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    <div className="container">
+      <div className="col">
+        <h1>Hi, I’m Shivesh</h1>A passionate frontend developer and a problem solver at heart. I specialize in creating
+        intuitive, user-focused web applications with clean, modern designs that perform seamlessly across devices.
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <div className="col">
+        <div className="row">
+          <h2>Skills and Expertise</h2>
+          Core Technologies: React, JavaScript (ES6+), HTML, CSS, Git. Frameworks/Libraries: Material UI, Tailwind CSS,
+          Styled Components. Tools: VS Code, GitHub, Notion, Figma (basic knowledge for design collaboration).
+          Additional Skills: Strong foundation in project management and research skills from my UPSC prep.
+        </div>
+        <div className="row">
+          <h2>Experience</h2>
+          Highlights Built several responsive and high-performing projects with React, focusing on clean, maintainable
+          code. Proficient in Material UI, and I’ve led updates for several complex UI components. Dedicated learner,
+          always exploring new technologies to keep my skills sharp and my projects efficient.
+        </div>
+      </div>
+      <div className="col">
+        When I’m not coding, you’ll probably find me diving into new books or staying fit with yoga. I’m a believer in
+        continuous improvement and always push myself to bring my best to every project.
+      </div>
+      <div className="col">
+        I’m excited to collaborate and bring ideas to life!
+        <button>Let’s connect</button>
+        to create something amazing together.
+      </div>
     </div>
   )
 }
